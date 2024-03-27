9fcc16662adaaf4773e72794be0e299792b7d1d1
insanecoder09 <147096842+insanecoder09@users.noreply.github.com>
2024-03-27 22:33:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const keys = [
 const keyDetails = {
     "dht-dct-ksy": {
         name: "Vivek",
-        isActive: true,
+        isActive: false,
     },
     "ahr-dhv-eut": {
         name: "Lala",
@@ -20,8 +20,49 @@ const keyDetails = {
     },
     "yet-kgt-eit": {
         name: "Lali",
-        isActive: true,
+        isActive: false,
     },
+}
+
+const getNewPrediction = () => {
+    var leftTemp, rightTemp;
+    leftTemp = Math.floor(Math.random() * 13) + 1
+
+    const findingRightCard = () => {
+        rightTemp = Math.floor(Math.random() * 13) + 1;
+        if (rightTemp == leftTemp) findingRightCard();
+    }
+    findingRightCard();
+    // leftTemp and rightTemp contain two non equal card numbers.
+
+
+    // Generating random symbols
+
+    let symbols = ["spades", "clubs", "diamonds", "hearts"];
+    const symbol1 = symbols[Math.floor(Math.random() * 4)];
+    const symbol2 = symbols[Math.floor(Math.random() * 4)];
+
+    // Generating file name.
+
+    var leftFileName, rightFileName;
+    if (leftTemp < 10) leftFileName = "0";
+    else leftFileName = "";
+    leftFileName =
+        "https://rummywinnner.com/wp-content/uploads/2024/03/" +
+        leftFileName +
+        leftTemp +
+        symbol1 +
+        ".png";
+    if (rightTemp < 10) rightFileName = "0";
+    else rightFileName = "";
+    rightFileName =
+        "https://rummywinnner.com/wp-content/uploads/2024/03/" +
+        rightFileName +
+        rightTemp +
+        symbol2 +
+        ".png";
+
+    return { leftFileName, rightFileName }
 }
 
 app.use(cors());
@@ -32,8 +73,18 @@ app.get('/', (req, res) => {
 
 app.get('/aniketBro/:key', (req, res) => {
     if (keys.includes(req.params.key))
-        res.send(keyDetails[req.params.key])
+        res.send(keyDetails[req.params.key]);
     else res.send('0')
 })
+
+app.get('/newPrediction/:key', (req, res) => {
+    if (keys.includes(req.params.key))
+    {
+        const Prediction = getNewPrediction();
+        res.send(Prediction);
+    }
+    else res.send('0')
+})
+
 
 app.listen(3000, console.log("Server running on port 3000"));
